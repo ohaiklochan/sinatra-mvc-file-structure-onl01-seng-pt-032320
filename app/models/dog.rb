4c43df6274cc087a0400a6c3842dc9737c73ed56
Chloe Steffens <caforonda103@yahoo.com>
2020-06-18 17:17:49 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,3 +1,12 @@
 class Dog
+  attr_accessor :name, :breed, :age
+  attr_reader
+  attr_writer
+  
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+  end
   
 end
